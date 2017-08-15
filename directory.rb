8387b51d828ba0fe0810e3696a48905400a7a740
Marcus Rands <mrands@hotmail.co.uk>
2017-08-15 18:18:03 +0000

    Changed students variable to a hash and updated print_students method

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 #All students listed in an array
 students = [
-    ["Dr. Hannibal Lecter", :november],
-    ["Darth Vader", :november],
-    ["Nurse Ratchet", :november],
-    ["Michael Corleone", :november],
-    ["Alex DeLarge", :november],
-    ["The Wicked Witch of the West", :november],
-    ["Terminator", :november],
-    ["Freddy Krueger", :november],
-    ["The Joker", :november],
-    ["Joffrey Baratheon",:november],
-    ["Norman Bates", :november]
-    ]
+    {name: "Dr. Hannibal Lecter",          cohort: :november},
+    {name: "Darth Vader",                  cohort: :november},
+    {name: "Nurse Ratchet",                cohort: :november},
+    {name: "Michael Corleone",             cohort: :november},
+    {name: "Alex DeLarge",                 cohort: :november},
+    {name: "The Wicked Witch of the West", cohort: :november},
+    {name: "Terminator",                   cohort: :november},
+    {name: "Freddy Krueger",               cohort: :november},
+    {name: "The Joker",                    cohort: :november},
+    {name: "Joffrey Baratheon",            cohort: :november},
+    {name: "Norman Bates",                 cohort: :november}
+]
 
 #And then printed.q
 def print_header
@@ -20,7 +20,7 @@ def print_header
 end
 def print_students(students)
   students.each do |student|
-    puts "#{student[0]} (#{student[1]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(students)

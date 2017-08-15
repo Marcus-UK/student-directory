a3fab16616f7eab51e621449166c11c677241bba
Marcus Rands <mrands@hotmail.co.uk>
2017-08-15 16:50:09 +0000

    Added cohort to students array. Updated print_students method to display the cohort.

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 #All students listed in an array
 students = [
-    "Dr. Hannibal Lecter",
-    "Darth Vader",
-    "Nurse Ratchet",
-    "Michael Corleone",
-    "Alex DeLarge",
-    "The Wicked Witch of the West",
-    "Terminator",
-    "Freddy Krueger",
-    "The Joker",
-    "Joffrey Baratheon",
-    "Norman Bates"
+    ["Dr. Hannibal Lecter", :november],
+    ["Darth Vader", :november],
+    ["Nurse Ratchet", :november],
+    ["Michael Corleone", :november],
+    ["Alex DeLarge", :november],
+    ["The Wicked Witch of the West", :november],
+    ["Terminator", :november],
+    ["Freddy Krueger", :november],
+    ["The Joker", :november],
+    ["Joffrey Baratheon",:november],
+    ["Norman Bates", :november]
     ]
 
 #And then printed.q
@@ -18,13 +18,13 @@ def print_header
   puts "The students of Villains Academy"
   puts "---------------"
 end
-def print_students(names)
-  names.each do |name|
-    puts name
+def print_students(students)
+  students.each do |student|
+    puts "#{student[0]} (#{student[1]} cohort)"
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students."
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students."
 end
 
 print_header

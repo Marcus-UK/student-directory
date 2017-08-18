811ece097d3f98fac868dd14fb3e7c973a97492f
Marcus Rands <mrands@hotmail.co.uk>
2017-08-18 12:13:07 +0000

    Finished exercise to add extra details to students list

--- directory.rb
+++ directory.rb
@@ -5,7 +5,8 @@ end
 def print_students(students) 
   i = 0
   while i < students.length do
-    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    puts "#{students[i][:name]} is #{students[i][:nationality]}. They are #{students[i][:height]} tall and they are
+     part of the #{students[i][:cohort]} cohort."
     i += 1
   end
 end
@@ -22,14 +23,35 @@ def input_students
   
   name = gets.chomp
   
+  puts "What is their nationality?"
+  
+  nationality = gets.chomp
+  
+  puts "And finally, how tall are they (in centimeters)?"
+  
+  height = gets.chomp
+  
+  
   while !name.empty? do
     #While the name variable is not empty, the following code is repeated. Breaks if nothing is entered.
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, nationality: nationality, height: height}
     puts "Now we have #{students.count} students"
+    puts "If necessary, enter another student's details below."
     name = gets.chomp
+    if name.empty?
+      students
+    else
+      puts "What is their nationality?"
+  
+        nationality = gets.chomp
+  
+      puts "And finally, how tall are they (in centimeters)?"
+  
+        height = gets.chomp
+    end
+    end  
+    students
   end
-  students
-end
 
 
 students = input_students  
